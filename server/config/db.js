1196b40288f99cf59392066eb29ddebfb0bdde1a
Tejinder <tejindersingh9170@gmail.com>
2022-10-29 23:33:50 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://tezz:comp229midtermtejinder@mongodbserver.ebrblui.mongodb.net/book_store?retryWrites=true&w=majority"
+};
